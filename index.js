1d9850c0f90945110fc15a2a7fc9bce474ef72cd
david thanphilom <davidthanphilom@msn.com>
2020-06-27 12:13:23 -0700

    coded and ready

---
+++ index.js
@@ -0,0 +1,21 @@
+
+//CHANGE THIS TO CSV FILE NAME
+const name = `pdps`;
+
+const fs = require('fs');
+const csvFilePath = `${name}.csv`
+const csv = require('csvtojson')
+csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+       // console.log(jsonObj);
+       const data = JSON.stringify(jsonObj);
+       // console.log(data);
+
+        fs.writeFile(`${name}.json`, data, (err) => {
+            if (err) throw err;
+            console.log(`The ${name} file has been saved!`);
+        });
+
+    })
+
